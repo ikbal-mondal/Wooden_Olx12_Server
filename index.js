8ef5112b6317348ceb176338691e88ce82537c3a
ikbal-mondal <kbalmondal062@gmail.com>
2022-11-24 18:09:24 +0530

    added .env file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 require('dotenv').config();
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -10,16 +10,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+async function run(){
 
+    try{
+        
+      
 
+    }
+    finally{
 
+    }
+
+}
 run().catch(error => console.log(error));
 
+
 app.get('/', async(req,res) => {
-    res.send('Doctors Portal server is running');
+    res.send('Wooden Olx  server is running');
     
 })
 
 app.listen(port, () => {
-    console.log('Doctors Portal running on ' + port);
+    console.log('Wooden Olx running on ' + port);
 })
