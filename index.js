6908985899a4564431379c2f8bc8c535427e2bb0
ikbal-mondal <kbalmondal062@gmail.com>
2022-11-24 18:32:05 +0530

    set mongodb id and password

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.WoodenDBUSerId}:${process.env.WoodenDBpassword}@cluster0.vcnuad9.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 async function run(){
 
     try{
-        
+
       
 
     }
