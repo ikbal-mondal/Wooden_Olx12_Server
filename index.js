038c54f3b4e903113c934d93267d64761b8dea17
ikbal-mondal <kbalmondal062@gmail.com>
2022-11-25 06:31:47 +0530

    change db collections

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.WoodenDBUSerId}:${process.env.WoodenDBpassword}@cluster0.vcnuad9.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.WoodenDBUSerId}:${process.env.WoodenDBpassword}@cluster0.yxnrlzt.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 async function run(){
       
     try{
      
-        const homeFurnitureCollection = client.db('WOODEN_OLX').collection('FurnitureCollectionForHome');
+        const FurnitureCategoriesCollection = client.db('WOODEN_OLX').collection('FurnitureCategories');
       
 
     }
